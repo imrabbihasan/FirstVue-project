e8547a17816a68006d836270fc26afb5ac95e2e9
MD RABBI HASAN <rabbihossain270@gmail.com>
2024-05-02 05:54:40 +0800

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>TO-DO List</h1>
+    <ul>
+      <li>
+        <to-do-item label="My ToDo Item"></to-do-item>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script> 
+import ToDoItem from './components/ToDoItem.vue';
+export default {
+  name:"app",
+  components: {
+    ToDoItem,
+  },
+};
+</script>
+
+<style>
+</style>
